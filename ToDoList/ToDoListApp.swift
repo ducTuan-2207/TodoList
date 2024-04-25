2e82f9c66e9618e37fdde8b7af1798ccb7a8c8ab
DucTuan <tuanhypc02@gmail.com>
2024-04-25 23:42:54 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by macOS on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
