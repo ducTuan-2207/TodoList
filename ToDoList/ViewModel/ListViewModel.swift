33ee0a6dd0f266279bee00bd9a750ff175f46673
DucTuan <tuanhypc02@gmail.com>
2024-04-29 16:39:37 +0700

    listViewModel

---
+++ ToDoList/ViewModel/ListViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ListViewModel.swift
+//  ToDoList
+//
+//  Created by macOS on 29/04/2024.
+//
+
+import Foundation
+
+class listViewModel {
+    @Published var items: [itemModel] = []
+    init() {
+        getItem()
+    }
+    func getItem(){
+        let newItem = [
+        itemModel(title: "This is the firts item", isComplete: false),
+        itemModel(title: "This is the 2 item", isComplete: true),
+        itemModel(title: "This is the 3 item", isComplete: false)
+    ]
+        items.append(contentsOf: newItem)
+    }
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    func moveItem(from: IndexSet, to: Int){
+        items.move(fromOffsets: from, toOffset: to)
+    }
+}
